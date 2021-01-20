db164c5b92fc82f28b4d386f1f54bbf431d36dd3
7oxicshadow <7oxicshadow@googlemail.com>
2021-01-20 14:16:51 +0000

    Update to use the correct ports and add missing LATENCY var

--- remote-player/receiver-audio.sh
+++ remote-player/receiver-audio.sh
@@ -12,14 +12,16 @@ ARTPSINKPORT=8100
 ARTCPSINKPORT=8101
 ARTCPSRCPORT=5105
 
+LATENCY=0
+
 AUDIO_CAPS="application/x-rtp,media=(string)audio,clock-rate=(int)48000,encoding-name=(string)OPUS"
 
 AUDIO_DEC="rtpopusdepay ! opusdec"
 AUDIO_SINK="audioconvert ! audioresample ! autoaudiosink"
 
 gst-launch-1.0 -v rtpbin name=rtpbin buffer-mode=none drop-on-latency=true latency=$LATENCY \
-     udpsrc caps=$AUDIO_CAPS port=$VRTPPORT address=$SRC multicast-iface=$SRCIF ! rtpbin.recv_rtp_sink_0 \
+     udpsrc caps=$AUDIO_CAPS port=$ARTPSINKPORT address=$SRC multicast-iface=$SRCIF ! rtpbin.recv_rtp_sink_0 \
        rtpbin. ! $AUDIO_DEC ! $AUDIO_SINK \
-     udpsrc port=$VRTCPPORT address=$SRC multicast-iface=$SRCIF ! rtpbin.recv_rtcp_sink_0 \
-       rtpbin.send_rtcp_src_0 ! udpsink port=$ARTCPSRCPORT host=$SRC multicast-iface=$SRCIF sync=false async=false 
+     udpsrc port=$ARTCPSRCPORT address=$SRC multicast-iface=$SRCIF ! rtpbin.recv_rtcp_sink_0 \
+       rtpbin.send_rtcp_src_0 ! udpsink port=$ARTCPSINKPORT host=$SRC multicast-iface=$SRCIF sync=false async=false 
 
